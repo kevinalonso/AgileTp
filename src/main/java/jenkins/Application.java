5d8a746301a1511f6d98d1b68ff38428f8f4a618
kevin <k.alonso@iia-laval.fr>
2018-06-07 11:35:24 +0200

    PMD

--- src/main/java/jenkins/Application.java
+++ src/main/java/jenkins/Application.java
@@ -1,16 +1,29 @@
 package jenkins;
 import org.joda.time.DateTime;
 
+/**
+ * My main class.
+ * @author kevin-pc.
+ */
 public class Application{
-	
+
+	/**
+	 * Entry point of application.
+	 * @param args argument from CLI.
+	 */
 	public static void main(String[] args){
 		System.out.println("Hello world");
 		DateTime date =  new DateTime().now();
 		System.out.println(date);
 	}
 	
+	/**
+	 * Resolve addition.
+	 * @param i first integer.
+	 * @param j second integer.
+	 * @return result of addition.
+	 */
 	public static int add(int i, int j) {
-		
 		return i + j;
 	}
 }
